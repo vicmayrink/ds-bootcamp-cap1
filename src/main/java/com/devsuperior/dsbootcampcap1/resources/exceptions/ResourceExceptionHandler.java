0136fe5a3be97ad1baa918c8cf31702869edc670
Victor Mayrink <mayrink.vtm@gmail.com>
2022-10-11 00:07:01 -0300

    Add standard error and exception handler

--- src/main/java/com/devsuperior/dsbootcampcap1/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/dsbootcampcap1/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,44 @@
 package com.devsuperior.dsbootcampcap1.resources.exceptions;
 
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dsbootcampcap1.services.exceptions.DatabaseException;
+import com.devsuperior.dsbootcampcap1.services.exceptions.ResourceNotFoundException;
+
+
+@ControllerAdvice
 public class ResourceExceptionHandler {
 
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> entityNotFound(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
 }
+
